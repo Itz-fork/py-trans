d62b46c0cbb592544f4d72735eb19d5375c6ca10
I'm Not A Bot #Left_TG <77770753+Itz-fork@users.noreply.github.com>
2021-09-12 23:44:42 +0530

    v0.2 -> v0.3

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ else:
 
 
 setup(name='py-trans',
-version='0.2',
+version='0.3',
 description='Free python library to translate text to different languages.',
 url='https://github.com/Itz-fork/py-trans',
 author='Itz-fork',
